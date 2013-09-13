2788c77f86370aab2679a02a648d73c6e2f502e2
Doug Hazell <doughazell@gmail.com>
2013-09-13 10:52:08 +0100

    Initial import of spreeBSC eCommerce site for Bespoke Silk Curtains Company

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeBSC::Application.config.secret_token = '7d284c20f60cc9335fd2d2a607ecdbd164e5e3d1cb196aa9d1dcf3a13714e4ca967813f17cf7c6038e1672147397f4d4f9f323d368a337a3ee27c6c2947a6dc4'
