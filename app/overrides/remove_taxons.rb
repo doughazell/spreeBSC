81dcedcb788ce637b84ef1e1bc5a892b6cdd5899
Doug Hazell <doughazell@gmail.com>
2013-10-07 11:45:42 +0100

    Porting BSC site from 'my_store' (based on Spree Sample) to 'spreeBSC'

---
+++ app/overrides/remove_taxons.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => 'spree/products/_taxons',
+                     :name => 'remove_taxons_list',
+                     :remove => "#taxon-crumbs[data-hook]")
+
+#
